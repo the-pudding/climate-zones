c1ca4a25291c873d2a124bc3d52321a91ed3e9a4
dktaylor916 <39200183+dktaylor916@users.noreply.github.com>
2024-03-22 23:01:38 +0000

    update github pages

---
+++ docs/_app/immutable/chunks/paths.CHZwt6ZR.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_532rj2?.base??"",a=globalThis.__sveltekit_532rj2?.assets??s;export{a,s as b};
--- src/actions/getColorSimp.js
+++ src/actions/getColorSimp.js
@@ -3,16 +3,16 @@ export function getColorSimp(DN) {
 		return "transparent";
 	}
 	if ((DN > 0) & (DN <= 3)) {
-		return "rgba(88, 152, 249)";
+		return "#1761fd";
 	}
 	if ((DN > 3) & (DN <= 7)) {
-		return "rgba(249, 88, 88)";
+		return "#ff8563";
 	}
 	if ((DN > 7) & (DN <= 16)) {
-		return "rgba(88, 249, 104)";
+		return "#abe340";
 	}
 	if ((DN > 16) & (DN <= 28)) {
-		return "rgba(178, 88, 249)";
+		return "#735fca";
 	}
 	if (DN > 28) {
 		return "white";
