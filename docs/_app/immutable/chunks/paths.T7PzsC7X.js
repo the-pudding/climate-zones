b75ff23010bd2da67a5d22f728c8c6875a2f90f6
Matthew Daniels <matthew.f.daniels@gmail.com>
2024-06-24 16:21:35 -0400

    update github pages

--- docs/_app/immutable/chunks/paths.T7PzsC7X.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1wwor8d?.base??"",a=globalThis.__sveltekit_1wwor8d?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/chunks/paths.UEqYJv55.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1tiendf?.base??"",e=globalThis.__sveltekit_1tiendf?.assets??s;export{e as a,s as b};
